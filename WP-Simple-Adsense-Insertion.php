06795d25f088e338cb81a375d00ed25e306c5fd8
mra13 <mra13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-21 09:59:45 +0000

    new shortcodes for placing adsense on the site and wp3.3 update
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-plugin-for-simple-google-adsense-insertion/trunk@478422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP-Simple-Adsense-Insertion.php
+++ WP-Simple-Adsense-Insertion.php
@@ -1,179 +1,202 @@
 <?php
-
 /*
 Plugin Name: WP Simple Adsense Insertion
-Version: v1.1
+Version: v1.4
 Plugin URI: http://www.tipsandtricks-hq.com/
 Author: Ruhul Amin
-Author URI: http://www.antique-hq.com/
-Plugin Description: A simple Wordpress plugin to insert Google Adsense into posts, pages, sidebars.
+Author URI: http://www.tipsandtricks-hq.com/
+Description: A simple Wordpress plugin to insert Google Adsense into posts, pages, sidebars.
+License: GPLv2
 */
 
 /*
-    This program is free software; you can redistribute it
-    under the terms of the GNU General Public License version 2,
-    as published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$wp_simple_ad_insert_version = 1.1;
+$wp_simple_ad_insert_version = 1.4;
 
+function show_ad_camp_1() {
+	$ad_camp_encoded_value_1 = get_option( 'wp_ad_camp_1_code' );
+	$ad_camp_decoded_value_1 = html_entity_decode( $ad_camp_encoded_value_1, ENT_COMPAT );
 
-function show_ad_camp_1()
-{
-    $ad_camp_encoded_value_1 = get_option('wp_ad_camp_1_code');
-    $ad_camp_decoded_value_1 = html_entity_decode($ad_camp_encoded_value_1, ENT_COMPAT);
-
-    if(!empty($ad_camp_decoded_value_1))
-    {
-        $output .= " $ad_camp_decoded_value_1";
-    }
-    return $output;
+	if ( !empty( $ad_camp_decoded_value_1 ) ) {
+		$output .= " $ad_camp_decoded_value_1";
+	}
+	return $output;
 }
 
-function show_ad_camp_2()
-{
-    $ad_camp_encoded_value_2 = get_option('wp_ad_camp_2_code');
-    $ad_camp_decoded_value_2 = html_entity_decode($ad_camp_encoded_value_2, ENT_COMPAT);
+add_shortcode( 'wp_ad_camp_1', 'show_ad_camp_1' );
 
-    if(!empty($ad_camp_decoded_value_2))
-    {
-        $output .= " $ad_camp_decoded_value_2";
-    }
-    return $output;
+function show_ad_camp_2() {
+	$ad_camp_encoded_value_2 = get_option( 'wp_ad_camp_2_code' );
+	$ad_camp_decoded_value_2 = html_entity_decode( $ad_camp_encoded_value_2, ENT_COMPAT );
+
+	if ( !empty( $ad_camp_decoded_value_2 ) ) {
+		$output .= " $ad_camp_decoded_value_2";
+	}
+	return $output;
 }
 
-function show_ad_camp_3()
-{
-    $ad_camp_encoded_value_3 = get_option('wp_ad_camp_3_code');
-    $ad_camp_decoded_value_3 = html_entity_decode($ad_camp_encoded_value_3, ENT_COMPAT);
+add_shortcode( 'wp_ad_camp_2', 'show_ad_camp_2' );
 
-    if(!empty($ad_camp_decoded_value_3))
-    {
-        $output .= " $ad_camp_decoded_value_3";
-    }
-    return $output;
+function show_ad_camp_3() {
+	$ad_camp_encoded_value_3 = get_option( 'wp_ad_camp_3_code' );
+	$ad_camp_decoded_value_3 = html_entity_decode( $ad_camp_encoded_value_3, ENT_COMPAT );
+
+	if ( !empty( $ad_camp_decoded_value_3 ) ) {
+		$output .= " $ad_camp_decoded_value_3";
+	}
+	return $output;
 }
 
-function wp_ad_camp_process($content)
-{
-    if (strpos($content, "<!-- wp_ad_camp_1 -->") !== FALSE)
-    {
-        $content = preg_replace('/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content);
-        $content = str_replace('<!-- wp_ad_camp_1 -->', show_ad_camp_1(), $content);
-    }
-    if (strpos($content, "<!-- wp_ad_camp_2 -->") !== FALSE)
-    {
-        $content = preg_replace('/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content);
-        $content = str_replace('<!-- wp_ad_camp_2 -->', show_ad_camp_2(), $content);
-    }
-    if (strpos($content, "<!-- wp_ad_camp_3 -->") !== FALSE)
-    {
-        $content = preg_replace('/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content);
-        $content = str_replace('<!-- wp_ad_camp_3 -->', show_ad_camp_3(), $content);
-    }
-    return $content;
+add_shortcode( 'wp_ad_camp_3', 'show_ad_camp_3' );
+
+function show_ad_camp_4() {
+	$ad_camp_encoded_value_4 = get_option( 'wp_ad_camp_4_code' );
+	$ad_camp_decoded_value_4 = html_entity_decode( $ad_camp_encoded_value_4, ENT_COMPAT );
+
+	if ( !empty( $ad_camp_decoded_value_4 ) ) {
+		$output .= " $ad_camp_decoded_value_4";
+	}
+	return $output;
 }
+
+add_shortcode( 'wp_ad_camp_4', 'show_ad_camp_4' );
+
+/**
+ * The wp_ad_camp_process function is deprecated.
+ * New users should use the updated shortcode method.
+ */
+
+function wp_ad_camp_process( $content ) {
+	if ( strpos( $content, "<!-- wp_ad_camp_1 -->" ) !== FALSE ) {
+		$content = preg_replace( '/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content );
+		$content = str_replace( '<!-- wp_ad_camp_1 -->', show_ad_camp_1(), $content );
+	}
+	if ( strpos( $content, "<!-- wp_ad_camp_2 -->" ) !== FALSE ) {
+		$content = preg_replace( '/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content );
+		$content = str_replace( '<!-- wp_ad_camp_2 -->', show_ad_camp_2(), $content );
+	}
+	if ( strpos( $content, "<!-- wp_ad_camp_3 -->" ) !== FALSE ) {
+		$content = preg_replace( '/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content );
+		$content = str_replace( '<!-- wp_ad_camp_3 -->', show_ad_camp_3(), $content );
+	}
+	if ( strpos( $content, "<!-- wp_ad_camp_4 -->" ) !== FALSE ) {
+		$content = preg_replace( '/<p>\s*<!--(.*)-->\s*<\/p>/i', "<!--$1-->", $content );
+		$content = str_replace( '<!-- wp_ad_camp_4 -->', show_ad_camp_4(), $content );
+	}
+	return $content;
+}
+
+add_filter( 'the_content', 'wp_ad_camp_process' );
 
 // Displays Simple Ad Campaign Options menu
 function ad_camp_add_option_page() {
-    if (function_exists('add_options_page')) {
-        add_options_page('Simple Adsense Insertion', 'Simple Adsense Insertion', 8, __FILE__, 'ad_insertion_options_page');
-    }
+	if ( function_exists( 'add_options_page' ) ) {
+		add_options_page( 'Simple Adsense Insertion', 'Adsense Insertion', 8, __FILE__, 'ad_insertion_options_page' );
+	}
 }
 
 function ad_insertion_options_page() {
 
-    global $wp_simple_ad_insert_version;
+	global $wp_simple_ad_insert_version;
 
-    if (isset($_POST['info_update']))
-    {
-        echo '<div id="message" class="updated fade"><p><strong>';
+	if ( isset( $_POST['info_update'] ) ) {
+		echo '<div id="message" class="updated fade"><p><strong>';
 
-        $tmpCode1 = htmlentities(stripslashes($_POST['wp_ad_camp_1_code']) , ENT_COMPAT);
-        update_option('wp_ad_camp_1_code', $tmpCode1);
+		$tmpCode1 = htmlentities( stripslashes( $_POST['wp_ad_camp_1_code'] ) , ENT_COMPAT );
+		update_option( 'wp_ad_camp_1_code', $tmpCode1 );
 
-        $tmpCode2 = htmlentities(stripslashes($_POST['wp_ad_camp_2_code']) , ENT_COMPAT);
-        update_option('wp_ad_camp_2_code', $tmpCode2);
+		$tmpCode2 = htmlentities( stripslashes( $_POST['wp_ad_camp_2_code'] ) , ENT_COMPAT );
+		update_option( 'wp_ad_camp_2_code', $tmpCode2 );
 
-        $tmpCode3 = htmlentities(stripslashes($_POST['wp_ad_camp_3_code']) , ENT_COMPAT);
-        update_option('wp_ad_camp_3_code', $tmpCode3);
+		$tmpCode3 = htmlentities( stripslashes( $_POST['wp_ad_camp_3_code'] ) , ENT_COMPAT );
+		update_option( 'wp_ad_camp_3_code', $tmpCode3 );
 
-        echo 'Options Updated!';
-        echo '</strong></p></div>';
-    }
+		$tmpCode4 = htmlentities( stripslashes( $_POST['wp_ad_camp_4_code'] ) , ENT_COMPAT );
+		update_option( 'wp_ad_camp_4_code', $tmpCode4 );
 
-    ?>
+		echo 'Options Updated!';
+		echo '</strong></p></div>';
+	}
 
-    <div class=wrap>
+?>
 
-    <h2>Simple Adsense Insertion Options v <?php echo $wp_simple_ad_insert_version; ?></h2>
+	<div class="wrap">
+		<h2>Adsense Insertion Options</h2>
 
-    <p>For information and updates, please visit:<br />
-    <a href="http://www.tipsandtricks-hq.com/?p=170">http://www.tipsandtricks-hq.com/</a></p>
+		<p>Use this plugin to quickly and easily insert Google Adsense to your posts or pages.</p>
+		<br>
+		
+	    <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+	    <input type="hidden" name="info_update" id="info_update" value="true" />
 
-    <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
-    <input type="hidden" name="info_update" id="info_update" value="true" />
+	    <fieldset class="options">
+	    <table width="100%" border="0" cellspacing="0" cellpadding="6">
 
-    <fieldset class="options">
-    <legend><strong>Usage</strong></legend>
-    <p>Use this plugin to quickly and easily insert Google Adsense to your posts, pages and sidebar.</p>
-    <p>There are two ways you can use this plugin:</p>
-    <ol>
-    <li>Add the trigger text <strong>&lt;!-- wp_ad_camp_1 --&gt;</strong><br />or <strong>&lt;!-- wp_ad_camp_2 --&gt;</strong><br />or <strong>&lt;!-- wp_ad_camp_3 --&gt;</strong>  to your posts or pages</li>
-    <li>Call the function from template files: <strong>&lt;?php echo show_ad_camp_1(); ?&gt;</strong><br />or <strong>&lt;?php echo show_ad_camp_2(); ?&gt;</strong><br />or <strong>&lt;?php echo show_ad_camp_3(); ?&gt;</strong></li>
-    </ol>
+	    <tr valign="top"><td width="35%" align="left">
+	    <strong>Adsense Ad Campaign 1 Code:</strong>
+	    <br>Copy and paste your adsense to the field on the right.
+	    <br>To display this ad use the shortcode: [wp_ad_camp_1]
+	    </td><td align="left">
+	    <textarea name="wp_ad_camp_1_code" rows="6" cols="50"><?php echo get_option( 'wp_ad_camp_1_code' ); ?></textarea>
+	    </td></tr>
 
-    </fieldset>
+	    <tr valign="top"><td width="35%" align="left">
+	    <strong>Adsense Ad Campaign 2 Code:</strong>
+	    <br>Copy and paste your adsense to the field on the right.
+	    <br>To display this ad use the shortcode: [wp_ad_camp_2]
+	    </td><td align="left">
+	    <textarea name="wp_ad_camp_2_code" rows="6" cols="50"><?php echo get_option( 'wp_ad_camp_2_code' ); ?></textarea>
+	    </td>
+	    </tr>
 
-    <fieldset class="options">
-    <legend><strong>Options</strong></legend>
+	    <tr valign="top"><td width="35%" align="left">
+	    <strong>Adsense Ad Campaign 3 Code:</strong>
+	    <br>Copy and paste your adsense to the field on the right.
+	    <br>To display this ad use the shortcode: [wp_ad_camp_3]
+	    </td><td align="left">
+	    <textarea name="wp_ad_camp_3_code" rows="6" cols="50"><?php echo get_option( 'wp_ad_camp_3_code' ); ?></textarea>
+	    </td></tr>
 
-    <table width="100%" border="0" cellspacing="0" cellpadding="6">
 
-    <tr valign="top"><td width="35%" align="left">
-    <strong>Adsense Ad Campaign 1 Code:</strong>
-    <br /><i>Copy and Paste the Google adsene code here. To show this add in your posts or pages use the token <br />&lt;!-- wp_ad_camp_1 --&gt;
-    <br />or call the function from a template file <br />&lt;?php echo show_ad_camp_1(); ?&gt;</i>
-    </td><td align="left">
-    <textarea name="wp_ad_camp_1_code" rows="6" cols="35"><?php echo get_option('wp_ad_camp_1_code'); ?></textarea>
-    </td></tr>
+	    <tr valign="top"><td width="35%" align="left">
+	    <strong>Adsense Ad Campaign 4 Code:</strong>
+	    <br>Copy and paste your adsense to the field on the right.
+	    <br>To display this ad use the shortcode: [wp_ad_camp_4]
+	    </td><td align="left">
+	    <textarea name="wp_ad_camp_4_code" rows="6" cols="50"><?php echo get_option( 'wp_ad_camp_4_code' ); ?></textarea>
+	    </td></tr>
 
-    <tr valign="top"><td width="35%" align="left">
-    <strong>Adsense Ad Campaign 2 Code:</strong>
-    <br /><i>Copy and Paste the Google adsene code here. To show this add in your posts or pages use the token <br />&lt;!-- wp_ad_camp_2 --&gt;
-    <br />or call the function from a template file <br />&lt;?php echo show_ad_camp_2(); ?&gt;</i>
-    </td><td align="left">
-    <textarea name="wp_ad_camp_2_code" rows="6" cols="35"><?php echo get_option('wp_ad_camp_2_code'); ?></textarea>
-    </td>
-    </tr>
+	    </table>
+	    </fieldset>
 
-    <tr valign="top"><td width="35%" align="left">
-    <strong>Adsense Ad Campaign 3 Code:</strong>
-    <br /><i>Copy and Paste the Google adsene code here. To show this add in your posts or pages use the token <br />&lt;!-- wp_ad_camp_3 --&gt;
-    <br />or call the function from a template file <br />&lt;?php echo show_ad_camp_3(); ?&gt;</i>
-    </td><td align="left">
-    <textarea name="wp_ad_camp_3_code" rows="6" cols="35"><?php echo get_option('wp_ad_camp_3_code'); ?></textarea>
-    </td></tr>
+	    <div class="submit">
+	        <input type="submit" name="info_update" value="<?php _e( 'Update options' ); ?> &raquo;" />
+	    </div>
 
-    </table>
-    </fieldset>
-
-    <div class="submit">
-        <input type="submit" name="info_update" value="<?php _e('Update options'); ?> &raquo;" />
-    </div>
-
-    </form>
-    </div><?php
+	    </form>
+	</div>
+<?php
 }
 
-add_filter('the_content', 'wp_ad_camp_process');
-
 // Insert the ad_camp_add_option_page in the 'admin_menu'
-add_action('admin_menu', 'ad_camp_add_option_page');
+add_action( 'admin_menu', 'ad_camp_add_option_page' );
 
+add_filter('the_content', 'do_shortcode');
+if (!is_admin())
+{add_filter('widget_text', 'do_shortcode');}
+add_filter('the_excerpt', 'do_shortcode');
 ?>
--- readme.txt
+++ readme.txt
@@ -1,48 +1,67 @@
 === WP Simple Adsense Insertion ===
+
 Contributors: Ruhul Amin
 Donate link: http://www.tipsandtricks-hq.com/
 Tags: Google Adsense, WordPress Plugin,
-Requires at least: 1.5
-Tested up to: 2.8.3
+Requires at least: 3.0
+Tested up to: 3.3
 Stable tag: 1.4
 
-Easy to use Wordpress plugin to insert Google Adsense to your posts, pages and sidebar.
+Easy to use Wordpress plugin to insert Google Adsense to your WordPress posts, pages and sidebar.
 
 == Description ==
+
 Use this plugin to quickly and easily insert Google Adsense to your posts, pages and sidebar by using a trigger text or calling the php function.
 
-There are many plugins and services which can add Google Adsense to your Wordpress site. However I found that even though something like Adsense Manager or Adsense Deluxe provides a lot of customizable options, it can be overwhelming and isn't really simple enough for people who are new to WordPress.
+There are many plugins and services which can add Google Adsense to your WordPress site. However I found that even though something like Adsense Manager or Adsense Deluxe provides a lot of customizable options, it can be overwhelming and isn't really simple enough for people who are new to WordPress.
 
-I found that most of the time I only use two or three types of Google Adsense units in various places and posts throughout my sites.That's why I wrote my own Simple Adsense Insertion Plugin for WordPress, to focus on having 1-3 Google Adsense codes saved and use them where ever I want on my site by inserting a simple trigger text to my posts,pages and sidebar.
+I found that most of the time I only use two or three types of Google Adsense units in various places and posts throughout my sites.That's why I wrote my own Simple Adsense Insertion Plugin for WordPress, to focus on having 1-4 Google Adsense codes saved and use them where ever I want on my site by inserting a simple trigger text to my posts,pages and sidebar.
 
 For information and updates, please visit:
 http://www.tipsandtricks-hq.com/?page_id=170
 
-Usage:
+= Usage: =
 
 There are two ways you can use this plugin:
 
-   1. Add the trigger text 
-         <!-- wp_ad_camp_1 -->
-      or <!-- wp_ad_camp_2 -->
-      or <!-- wp_ad_camp_3 --> to your posts or pages
-   2. Call the function from template files:
-         <?php echo show_ad_camp_1(); ?>
-      or <?php echo show_ad_camp_2(); ?>
-      or <?php echo show_ad_camp_3(); ?>
+Use the shortcodes: 
 
+* [wp_ad_camp_1]
+* [wp_ad_camp_2]
+* [wp_ad_camp_3]
+* [wp_ad_camp_4]
+
+Call the function from template files:
+
+* <?php echo show_ad_camp_1(); ?>
+* <?php echo show_ad_camp_2(); ?>
+* <?php echo show_ad_camp_3(); ?>
+* <?php echo show_ad_camp_4(); ?>
 
 == Installation ==
 
 1. Unzip and Upload the folder 'WP-Simple-Adsense-Insertion' to the '/wp-content/plugins/' directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Go to Settings and configure the options eg. Copy and paste the Google Adsense code that you want to use.
-4. Add the trigger text <!-- wp_ad_camp_1 --> to a post or page where u want it to appear.
+4. Add the trigger text [wp_ad_camp_1] to a post or page where you want it to appear.
 
 == Frequently Asked Questions ==
-1. Does this plugin allow complex adsense management? 
+
+= Does this plugin allow complex adsense management? =
+
 No.
 
+= I am updating an older version of the plugin.  Will it still work? =
+
+The method for displaying ads changed in version 1.2.  The old style will continue to work in this version, but it highly suggested that you update all the adcode in your content to the new method.
+
+An easy way to do this would be to install the find/replace plugin (http://wordpress.org/extend/plugins/search-and-replace/) and update all your old tags.  E.g. Find <!-- wp_ad_camp_1 --> and replace it with [wp_ad_camp_1].
+
 == Screenshots ==  
+
 1. Check out this Plugin in action at http://www.tipsandtricks-hq.com
 
+== Changelog ==
+
+Please see this page for changelog:
+http://www.tipsandtricks-hq.com/wordpress-plugin-for-simple-google-adsense-insertion-170
